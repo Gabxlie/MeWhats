9a4fea737bab765ea25964051df6c8355abbf11e
Gabxlie <116379065+Gabxlie@users.noreply.github.com>
2022-10-21 13:30:48 -0500

    Add files via upload

---
+++ clogs.js
@@ -0,0 +1,128 @@
+{
+    "packages": [
+        {   
+            "version": "12.0",
+            "package": "com.smwhatsapp",
+            "status": "update_title",
+            "title": "WhatsApp {SM} 12",
+            "note": "false",
+            "msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html",
+            "download": "https://crismod.com/whatsapp/com.whatsapp",
+            "update": [
+                {
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/dark/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/dark/changelog-es.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/light/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/light/changelog-es.html"
+				}
+            ]
+        },
+        {
+            "version": "12.0",
+            "package": "com.whatsapp",
+            "status": "update_title",
+            "title": "WhatsApp {SM} 12",
+            "note": "false",
+            "msgtxt": "https://raw.githubusercontent.com/CrisMod/up_system/main/business/tips.html",
+            "download": "https://t.me/wamodcris",
+            "update": [
+                {
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/dark/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/dark/changelog-es.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/light/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/light/changelog-es.html"
+				}
+            ]
+        },
+                {   
+            "version": "12.0",
+            "package": "com.wampro",
+            "status": "update_title",
+            "title": "WhatsApp {SM} 12",
+            "note": "false",
+            "msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html",
+            "download": "https://www.mediafire.com/file/194u1st85gftlo9/WaM_23.10.apk/file",
+            "update": [
+                {
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/dark/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/dark/changelog-es.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/light/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/light/changelog-es.html"
+				}
+            ]
+        },
+                {   
+            "version": "15.10",
+            "package": "com.whatsapp.w4b",
+            "status": "update_title",
+            "title": "Wa{Business M} 15.10",
+            "note": "false",
+            "msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html",
+            "download": "https://crismod.com/business/com.whatsapp.w4b",
+            "update": [
+                {
+					"languaje": "es",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/business/dark/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/SM/business/changelog-es.html"
+				},
+				{
+					"languaje": "es",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/business/light/changelog-es.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/business/light/changelog-es.html"
+				}
+            ]
+        }
+    ]
+}
