2bfebb68e6eb5a49fbadae7dfd539e574fc741c8
Gabxlie <116379065+Gabxlie@users.noreply.github.com>
2022-10-28 18:01:22 -0500

    Update clogs.js

--- clogs.js
+++ clogs.js
@@ -83,4 +83,29 @@
             ]
         }
     ]
-}
+},
+{
+    "packages": [
+        {   
+            "version": "7.1",
+            "package": "com.instagram.android",
+            "status": "update_title",
+            "title": "InstaMe+ 7.1",
+            "note": "false",
+            "msgtxt": "https://raw.githubusercontent.com/Gabxlie/MeWhats/main/MeWhatsApp/dark/changelog-es.html",
+            "download": "https://mewhatsapp-plus.blogspot.com/2022/09/mewhatsapp-v60.html?m=1",
+            "update": [
+             		{
+					"languaje": "en",
+					"theme": "dark",
+					"changelog": "https://raw.githubusercontent.com/Gabxlie/MeWhats/main/MeWhatsApp/dark/changelog-en.html"
+				},
+				{
+					"languaje": "en",
+					"theme": "light",
+					"changelog": "https://raw.githubusercontent.com/Gabxlie/MeWhats/main/MeWhatsApp/light/changelog-en.html"
+				}
+			 
+            ]
+        },
+
